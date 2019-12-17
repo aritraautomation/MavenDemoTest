0288f1147132e9e8a5c378c43e35e70c2fea0ec5
aritrasingha <aritras@rssoftware.co.in>
2019-12-17 12:37:41 +0530

    add one class

---
+++ src/test/java/org/qa/seleniumDemo/DemoTest.java
@@ -0,0 +1,15 @@
+package org.qa.seleniumDemo;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public DemoTest() {
+		
+		
+	}
+
+}
